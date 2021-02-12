ea242e501ec440743c6c4688c6d6974194587bf1
namantia1@gmail.com <namantia1@gmail.com>
2021-02-12 16:08:29 -0500

    deleted ImageDisplay component / renders images conditionally due to weather type

--- src/components/ImageDisplay/ImageDisplay.js
+++ src/components/ImageDisplay/ImageDisplay.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import main from '../../assests/images/main.jpg';
+import snow from '../../assests/images/snow.jpg';
+import rain from '../../assests/images/rain.jpg';
 import '../../css/main.css';
 
-const ImageDisplay = () => {
-  const [bgImage, setBgImage] = useState(main);
+const ImageDisplay = (props) => {
+  const [bgImage, setBgImage] = useState('');
+
+  if (props.currentWeather === 'Snow') {
+    setBgImage(snow);
+  } else if (props.currentWeather === 'Rain') {
+    setBgImage(rain);
+  } else {
+    setBgImage(main);
+  }
   return (
     <div>
-      <p className="test">ImageDisplay.js</p>
       <div className="image-display-fit-container">
         <img className="image-display-fit" src={bgImage} alt="" />
       </div>
--- src/components/WeatherApi/WeatherApi.js
+++ src/components/WeatherApi/WeatherApi.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../../css/main.css';
+import main from '../../assests/images/main.jpg';
+import snow from '../../assests/images/snow.jpg';
+import rain from '../../assests/images/rain.jpg';
 import WeatherDisplay from '../WeatherDisplay/WeatherDisplay';
 import ImageDisplay from '../ImageDisplay/ImageDisplay';
 
@@ -8,6 +11,7 @@ const WeatherApi = (props) => {
   const [currentTemp, setCurrentTemp] = useState('');
   const [currentWeather, setCurrentWeather] = useState('');
   const [currentLocation, setCurrentLocation] = useState('');
+  const [bgImage, setBgImage] = useState(main);
 
   // Pulls data from the Api then sets the vatiables with the data
   const handleSubmit = () => {
@@ -29,6 +33,17 @@ const WeatherApi = (props) => {
       // Alert user if they request weather info when the input is empty
       alert('Please Enter A Zip Code');
     }
+    handleImage();
+  };
+
+  const handleImage = () => {
+    if (currentWeather === 'Snow') {
+      setBgImage(snow);
+    } else if (currentWeather === 'Rain') {
+      setBgImage(rain);
+    } else {
+      setBgImage(main);
+    }
   };
   return (
     <div>
@@ -46,8 +61,12 @@ const WeatherApi = (props) => {
         currentWeather={currentWeather}
         currentLocation={currentLocation}
       />
-
-      <ImageDisplay />
+      <div className="image-display-fit-container">
+        <img className="image-display-fit" src={bgImage} alt="" />
+      </div>
+      <a href="https://www.freepik.com/vectors/icons">
+        Icons vector created by titusurya - www.freepik.com
+      </a>
     </div>
   );
 };
